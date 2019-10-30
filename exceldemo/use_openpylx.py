c843c84fb0e20c08d71934ca24281c0c4b6ef4c8
RustFisher <rustfisher@foxmail.com>
2019-10-30 11:27:46 +0800

    [openpyxl] demo

--- exceldemo/use_openpylx.py
+++
@@ -1,3 +0,0 @@
-# Read write excel files.
-# https://openpyxl.readthedocs.io/en/stable/index.html#module-openpyxl
-
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Python playground
 
 - [ ] openpyxl 示例
+    - [x] 新建Excel文件
+    - [ ] 读取并修改保存Excel
+    
