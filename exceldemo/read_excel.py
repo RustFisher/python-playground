22b7bd71f514b2c9f14b5e7dca0aa5a11f394ad5
RustFisher <rustfisher@foxmail.com>
2019-11-05 20:41:47 +0800

    excel

--- exceldemo/read_excel.py
+++ exceldemo/read_excel.py
@@ -1,6 +1,9 @@
 from copy import copy
 
 from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+
+from exceldemo import create_data_excel
 
 
 def read_xlsx_basic(file_path):
@@ -33,11 +36,29 @@ def change_title_line_format(file_path):
         font.size = 13
         font.bold = True
         cell.font = font
+
+        if col > 1:
+            cell.alignment = Alignment(horizontal="center", vertical="center")  # 居中显示
+
+    # 调整数据单元格
+    for row in range(2, st.max_row + 1):
+        for col in range(2, st.max_column + 1):
+            cell = st.cell(row=row, column=col)
+            cell.alignment = Alignment(horizontal="center", vertical="center")  # 居中显示
+
+    # 调整单元格的格式 - 显示百分比
+    for row in range(2, st.max_row + 1):
+        for col in range(4, st.max_column + 1):
+            cell = st.cell(row=row, column=col)
+            cell.number_format = '0%'
+
+    st.cell(row=2, column=7).value = 0.42  # 修改数值
     wb.save(file_path)
 
 
 if __name__ == '__main__':
     excel_path = 'out/指数.xlsx'
+    create_data_excel.create_excel_demo(excel_path)
     ws1 = load_workbook(excel_path).active
     if ws1 is None:
         print('表格不存在')
